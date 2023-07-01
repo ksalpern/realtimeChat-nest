0d2dda78abf8204ef0161fe56301dd8e0e1d5c34
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-01 14:29:13 +0300

    client html endpoint

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -1,9 +1,39 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Prisma, Chat } from '@prisma/client';
+import { AppService } from './../app.service';
 
 @WebSocketGateway()
-export class AppGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private appService: AppService) {}
+  @WebSocketServer() server: Server;
+  @SubscribeMessage('sendMessage')
+  async handleSendMessage(
+    client: Socket,
+    payload: Prisma.ChatCreateInput,
+  ): Promise<void> {
+    await this.appService.createMessage(payload);
+    this.server.emit('recMessage', payload);
+  }
+
+  afterInit(server: any) {
+    console.log(server)
+  }
+
+  handleConnection(client: Socket) {
+    console.log(`Connected: ${client.id}`)
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Disconnected: ${client.id}`)
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors()
+  app.useStaticAssets(join(__dirname,'..','static'))
+  app.setBaseViewsDir(join(__dirname,'..','views'))
+  app.setViewEngine('ejs')
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app)
-  
+
   await app.listen(3000);
 }
 bootstrap();
